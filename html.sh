3e021e2265a2ce559c94eb8c1ee7f67f40a7671b
Ramya595 <ramyalatha595@gmail.com>
2025-05-28 19:42:51 +0530

    Update html.sh

--- html.sh
+++ html.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 echo " commands in git bash"
-echo " which ---- to find location where it is download  like which bash"
+echo " command flags arguments ----Syntax"
+echo " which ---- to find location(path) where it is download  like which bash"
 echo " command(vim) arguments (html.sh) file name ---to write & make chages in file "
 echo " cat -- print the content of the file"
 echo "touch ---to create the file"
@@ -8,5 +9,12 @@ echo "mkdir--- to make the directory"
 echo " cd-- to change the directory"
 echo "command(ls) flags(-l) arguments() "
 echo " command(pyhton) arguments(filename)---to run the file in python"
+echo " Pyhton---directly enter in to that language write &run her--> command - exit() to get back"
+echo " ./filename-----executing any file  ,  ./---present directory"
+echo " ls(list of cmds ) -ali    a--all  l--list  i-- information"
+echo " pwd -- present location or path(print working directory)"
+
+
+
 
 
