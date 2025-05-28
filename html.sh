173be2b681a9c597579f173f6979fdab0c8dc0ce
Ramya595 <ramyalatha595@gmail.com>
2025-05-28 12:58:47 +0530

    Add files via upload

---
+++ html.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+echo " commands in git bash"
+echo " which ---- to find location where it is download  like which bash"
+echo " command(vim) arguments (html.sh) file name ---to write & make chages in file "
+echo " cat -- print the content of the file"
+echo "touch ---to create the file"
+echo "mkdir--- to make the directory"
+echo " cd-- to change the directory"
+echo "command(ls) flags(-l) arguments() "
+echo " command(pyhton) arguments(filename)---to run the file in python"
+
+
