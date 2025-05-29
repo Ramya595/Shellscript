1fdcd5e22c2fbf93b3643faa11e5b940b9ec4d7e
Ramya <ramyalatha595@gmail.com>
2025-05-29 16:36:47 +0530

    file updated

--- EO.sh
+++ EO.sh
@@ -3,9 +3,9 @@
 echo " enter any number: " 
 read -p "number:"
  
-z= `exp $number % 2`
-echo"$z"
-if [ $z -eq 0 ];then
+x=2
+
+if [ $((number % x)) -eq 0 ];then
 echo "given number is even"
 else
 echo "given number is odd"
