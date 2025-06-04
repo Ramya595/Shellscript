1b26f9268912cef5f212a6cfd37f50de1ace1ab8
Ramya <ramyalatha595@gmail.com>
2025-06-04 21:45:56 +0530

    files added

---
+++ file1.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+
+read -p "enter the username " username
+read -p "enter the age " age
+echo " $username are $age old"
+
+if [ $username = "admin" ]; then
+    echo "your are admin"
+    else 
+    echo " you are $username"
+    fi
+
+if [ $age -lt 13 ]; then
+echo "no access"
+elif [ $age -lt 18 ]; then
+echo "limited access"
+else
+echo "full access"
+fi
+    
